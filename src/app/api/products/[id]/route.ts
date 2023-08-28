c3ac4576a0e339b8c1be98b70efe72e6ba81afef
AnasOnGit <50570153+AnasOnGit@users.noreply.github.com>
2023-08-28 22:32:37 +0300

    fixing deploying errors

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,15 +1,31 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 
- 
-export async function GET(request: Request,{params}:{params: {id: string}}) {
-  let productsRes = await fetch(`${process.env.NEXT_URL}/products.json`);
-  let products:any = await productsRes.json(); 
-  const product = products.find((p :any)  => p.id === parseInt(params.id));
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  try {
+    // Fetch the products JSON data
+    const productsRes = await fetch(`${process.env.NEXT_URL}/products.json`);
+
+    // Check if the fetch request was successful
+    if (!productsRes.ok) {
+      // Handle the case when the fetch request is not successful (e.g., 404 Not Found)
+      return NextResponse.json({ message: "Error fetching products" }, { status: 500 });
+    }
+
+    // Parse the JSON data
+    const products: any = await productsRes.json();
+
+    // Find the product with the specified ID
+    const product = products.find((p: any) => p.id === parseInt(params.id));
 
     if (product) {
       // If a matching product is found, return it as JSON
       return NextResponse.json(product, { status: 200 });
     } else {
-      return NextResponse.json({message:"No products found!"}, { status: 404 });
+      return NextResponse.json({ message: "Product not found" }, { status: 404 });
     }
+  } catch (error) {
+    // Handle any other errors that might occur
+    console.error("Error:", error);
+    return NextResponse.json({ message: "An error occurred" }, { status: 500 });
+  }
 }
--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
 
-
 interface Product {
   title: string;
   category: string;
@@ -8,44 +7,55 @@ interface Product {
 }
 
 export async function GET(request: any) {
-  let productsRes = await fetch(`${process.env.NEXT_URL}/products.json`);
-  let products:any = await productsRes.json();
-  // Extract the search term and category from the query parameters
-  const searchTerm = request.nextUrl.searchParams.get('search-term');
-  const category = request.nextUrl.searchParams.get('category');
+  try {
+    // Fetch the products JSON data
+    const productsRes = await fetch(`${process.env.NEXT_URL}/products.json`);
+    
+    // Check if the fetch request was successful
+    if (!productsRes.ok) {
+      // Handle the case when the fetch request is not successful (e.g., 404 Not Found)
+      return NextResponse.json({ message: "Error fetching products" }, { status: 500 });
+    }
 
-  // Split the search term into individual words
-  const searchWords = searchTerm ? searchTerm.split(" ") : [];
+    // Parse the JSON data
+    const products: Product[] = await productsRes.json();
 
-  // Perform the search
-  const searchResults = searchProducts(products, searchWords, category);
+    // Extract the search term and category from the query parameters
+    const searchTerm = request.nextUrl.searchParams.get('search-term');
+    const category = request.nextUrl.searchParams.get('category');
 
-  // Check if there are search results
-  if (searchResults.length > 0) {
-    // Return the search results as JSON with a 200 status code
-    console.log(searchResults, { status: 200 });
-    return NextResponse.json(searchResults, { status: 200 });
-  } else {
-    // Return an empty JSON response with a 404 status code
-    return NextResponse.json({message:"No products found!"}, { status: 404 });
-  }
-  // Function to filter products based on the search term and category
-  function searchProducts(products: Product[], searchWords: string[], category: string | null): Product[] {
-    return products.filter(product => {
-      // Check if any word in the search term is found in the product title
-      const matchesSearchTerm = searchWords.length === 0 ||
-        searchWords.some(word => product.title.toLowerCase().includes(word.toLowerCase()));
-  
-      // Check if the product belongs to the specified category
-      const matchesCategory = !category || product.category.toLowerCase() === category.toLowerCase();
-  
-      // Return true if both conditions are met
-      return matchesSearchTerm && matchesCategory;
-    });
+    // Split the search term into individual words
+    const searchWords = searchTerm ? searchTerm.split(" ") : [];
+
+    // Perform the search
+    const searchResults = searchProducts(products, searchWords, category);
+
+    // Check if there are search results
+    if (searchResults.length > 0) {
+      // Return the search results as JSON with a 200 status code
+      return NextResponse.json(searchResults, { status: 200 });
+    } else {
+      // Return an empty JSON response with a 404 status code
+      return NextResponse.json({ message: "No products found!" }, { status: 404 });
+    }
+
+  } catch (error) {
+    // Handle any other errors that might occur
+    console.error("Error:", error);
+    return NextResponse.json({ message: "An error occurred" }, { status: 500 });
   }
 }
+// Function to filter products based on the search term and category
+function searchProducts(products: Product[], searchWords: string[], category: string | null): Product[] {
+  return products.filter(product => {
+    // Check if any word in the search term is found in the product title
+    const matchesSearchTerm = searchWords.length === 0 ||
+      searchWords.some(word => product.title.toLowerCase().includes(word.toLowerCase()));
 
+    // Check if the product belongs to the specified category
+    const matchesCategory = !category || product.category.toLowerCase() === category.toLowerCase();
 
-
-
-
+    // Return true if both conditions are met
+    return matchesSearchTerm && matchesCategory;
+  });
+}
